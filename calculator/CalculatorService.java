97ec21caa8989876f0233ee8f634e3db18bca93a
Vita <Vitayliyost@mail.ru>
2022-03-11 02:43:05 +0300

    выполнила ДЗ 2.4

---
+++ calculator/CalculatorService.java
@@ -0,0 +1,10 @@
+package pro.sky.course2.calculator.calculator;
+
+public interface CalculatorService {
+    int plus(int num1, int num2);
+    int minus(int num1, int num2);
+
+    int divide(int num1, int num2);
+
+    int multiply(int num1, int num2);
+}
